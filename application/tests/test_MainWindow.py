6eee32874bf0eace2842ede7b4da0af8f6a126a8
Pedrossann <petrnemec123@centrum.cz>
2023-12-27 18:34:09 +0100

    creating test for MainWindow

--- application/tests/test_MainWindow.py
+++ application/tests/test_MainWindow.py
@@ -1,15 +1,44 @@
 import unittest
+from unittest.mock import patch
 
 from application.src.MainWindow import MainWindow
 
 
 class TestMainWindow(unittest.TestCase):
     def setUp(self):
-        self.app = MainWindow()
+        with patch('application.src.MainWindow.Footer') as MockFooter, \
+             patch('application.src.MainWindow.Header') as MockHeader, \
+             patch('application.src.MainWindow.Body') as MockBody, \
+             patch('application.src.MainWindow.ExcelInput') as MockExcelInput, \
+             patch('application.src.MainWindow.ExcelOutput') as MockExcelOutput, \
+             patch('application.src.MainWindow.FormulasHandler') as MockFormulasHandler:
+
+            self.mock_footer = MockFooter
+            self.mock_header = MockHeader
+            self.mock_body = MockBody
+            self.mock_excel_input = MockExcelInput
+            self.mock_excel_output = MockExcelOutput
+            self.mock_formulas_handler = MockFormulasHandler
+
+            self.app = MainWindow()
 
     def testInit(self):
-        self.assertIsNone(self.app)
+        self.assertIsNotNone(self.app)
 
+    def testInitializingObjects(self):
+        self.mock_footer.assert_called_once()
+        self.mock_header.assert_called_once()
+        self.mock_body.assert_called_once()
+        self.mock_excel_input.assert_called_once()
+        self.mock_excel_output.assert_called_once()
+        self.mock_formulas_handler.assert_called_once()
+
+        self.assertEquals(self.app.footer, self.mock_footer.return_value)
+        self.assertEquals(self.app.header, self.mock_header.return_value)
+        self.assertEquals(self.app.body, self.mock_body.return_value)
+        self.assertEquals(self.app.excel_input, self.mock_excel_input.return_value)
+        self.assertEquals(self.app.excel_output, self.mock_excel_output.return_value)
+        self.assertEquals(self.app.formulas_handler, self.mock_formulas_handler.return_value)
 
 if __name__ == "__main__":
     unittest.main()
